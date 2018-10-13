13581f58bdc787ae8799099152d97f56753c9cd4
cmolinaord <carlosmolina.ord@gmail.com>
2018-10-13 02:18:08 +0200

    Improved plotting functions

--- plot.py
+++ plot.py
@@ -1,17 +1,60 @@
+import matplotlib as mpl
+from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import pyplot as plt
+import process_data as pd
+import numpy as np
+from numpy import pi, sin, cos, deg2rad
 
-fig, (ax1, ax2) = plt.subplots(2,1)
-ax1.plot(gpspos_time, knots, 'o-b')
-ax1.set_xlabel('time (s)')
-ax1.set_ylabel('Speed (knots)', color='b')
-ax1.tick_params('y', colors='b')
-ax12 = ax1.twinx()
-ax12.plot(atm_time, baro_alt, 'r')
-ax12.set_ylabel('Altitude (m)', color='r')
-ax12.tick_params('y', colors='r')
+mpl.rcParams['legend.fontsize'] = 10
 
-ax2.plot(head_time, heading[:,0])
-ax2.set_xlabel('time (s)')
-ax2.set_ylabel('Heading')
+time, alt0 = pd.resample("flight_1", 3, 'baro_alt', 2*60, 4*60, 0.5)
+alt0 = np.average(alt0)
+
+t0 = 13.4 # minutes
+t0 = t0 * 60
+
+tf = 15.8
+tf = tf * 60
+
+dt = 0.5
+time, lat = pd.resample("flight_1", 1, 'lat', t0, tf, dt)
+time, lon = pd.resample("flight_1", 1, 'lon', t0, tf, dt)
+time, alt = pd.resample("flight_1", 3, 'baro_alt', t0, tf, dt)
+time, h = pd.resample("flight_1", 4, 'z', t0, tf, dt)
+alt = alt - alt0
+lat = pd.deg2meters(lat) - pd.deg2meters(lat[0])
+lon = pd.deg2meters(lon) - pd.deg2meters(lon[0])
+x = pd.deg2meters(cos(h))
+y = pd.deg2meters(sin(h))
+
+DT = 4
+t, lat_v = 	pd.resample("flight_1", 1, 'lat', t0, tf, DT)
+t, lon_v = 	pd.resample("flight_1", 1, 'lon', t0, tf, DT)
+t, alt_v = 	pd.resample("flight_1", 3, 'baro_alt', t0, tf, DT)
+t, head = pd.resample("flight_1", 4, 'x', t0, tf, DT)
+alt_v = alt_v - alt0
+head = deg2rad(head)
+lat_v = pd.deg2meters(lat_v) - pd.deg2meters(lat_v[0])
+lon_v = pd.deg2meters(lon_v) - pd.deg2meters(lon_v[0])
+X = pd.deg2meters(cos(head))
+Y = pd.deg2meters(sin(head))
+
+fig1, (ax1, ax2) = plt.subplots(1,2)
+ax1.plot(lon, lat, '-b', label='path')
+ax1.quiver(lon, lat, x, y, label='heading')
+ax1.set_xlabel('longitude (deg)')
+ax1.set_ylabel('latitude (deg)')
+ax1.legend()
+
+ax2.plot(time, alt, '.-r')
+ax2.set_xlabel('Time (s)')
+ax2.set_ylabel('Altitude (m)')
+
+fig2 = plt.figure()
+ax21 = fig2.gca(projection='3d')
+ax21.plot(lon, lat, alt, '-r', label='Flight path')
+ax21.plot(lon_v, lat_v, alt_v, 'ob')
+#ax21.quiver(lon_v, lat_v, alt_v, X, Y, 0 )
+ax21.legend()
 
 plt.show()
