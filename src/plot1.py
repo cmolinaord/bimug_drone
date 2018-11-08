Corrected plots to calculate the heading in radians

src/plot1.py
@@ -13,8 +13,8 @@ time, alt0 = pd.resample(comment, 3, 'baro_alt', 2*60, 4*60, 0.5)
 alt0 = np.average(alt0)
 
 dt = 0.5
-t0 = 800
-tf = 920
+t0 = 800.1
+tf = 920.1
 
 dt = 0.5
 t, lat = pd.resample(comment, 1, 'lat', t0, tf, dt)
@@ -24,8 +24,9 @@ t, h = pd.resample(comment, 4, 'x', t0, tf, dt)
 alt = alt - alt0
 lat = pd.deg2meters(lat) - pd.deg2meters(lat[0])
 lon = pd.deg2meters(lon) - pd.deg2meters(lon[0])
-x = pd.deg2meters(cos(h))
-y = pd.deg2meters(sin(h))
+h = deg2rad(h)
+x = 0.2*sin(h)
+y = 0.2*cos(h)
 
 DT = 10
 T, lat_v = 	pd.resample(comment, 1, 'lat', t0, tf, DT)
@@ -33,11 +34,11 @@ T, lon_v = 	pd.resample(comment, 1, 'lon', t0, tf, DT)
 T, alt_v = 	pd.resample(comment, 3, 'baro_alt', t0, tf, DT)
 T, head = pd.resample(comment, 4, 'x', t0, tf, DT)
 alt_v = alt_v - alt0
-head = deg2rad(head)
+headd = deg2rad(head)
 lat_v = pd.deg2meters(lat_v) - pd.deg2meters(lat_v[0])
 lon_v = pd.deg2meters(lon_v) - pd.deg2meters(lon_v[0])
-X = pd.deg2meters(cos(head))
-Y = pd.deg2meters(sin(head))
+X = pd.deg2meters(cos(headd))
+Y = pd.deg2meters(sin(headd))
 
 # Figure 1 (Path in 2D)
 #fig1, (ax1, ax2) = plt.subplots(1,2)